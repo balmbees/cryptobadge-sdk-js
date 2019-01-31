b8e7d2cad1abfa73abefef78aa5d606b7bf1217c
GenesisSam <samuel940401@gmail.com>
2019-01-31 14:33:15 +0900

    Add polyfill

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,9 @@ import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import * as Crypto from "crypto";
+// node-fetch polyfill
+import "whatwg-fetch";
+
 import fetch from "node-fetch";
 
 import { Queries } from "./queries";
@@ -13,25 +16,34 @@ export class CryptobadgeClient extends Queries<NormalizedCacheObject> {
   private accessToken: string;
   private authorizationKey: string | null;
 
-  constructor(options: {
-    url?: string,
-    accessToken?: string,
-    authorizationKey?: string,
-  } = {}) {
+  constructor(
+    options: {
+      url?: string;
+      accessToken?: string;
+      authorizationKey?: string;
+    } = {},
+  ) {
     super();
 
-    this.url = options.url || process.env.CRYPTOBADGE_API_URL || "https://api.cryptobadge.app/graphql";
+    this.url =
+      options.url ||
+      process.env.CRYPTOBADGE_API_URL ||
+      "https://api.cryptobadge.app/graphql";
     this.accessToken = (() => {
-      const accessToken = options.accessToken || process.env.CRYPTOBADGE_ACCESS_TOKEN;
+      const accessToken =
+        options.accessToken || process.env.CRYPTOBADGE_ACCESS_TOKEN;
       if (!accessToken) {
         throw new Error("CRYPTOBADGE_ACCESS_TOKEN is required");
       }
       return accessToken;
     })();
-    this.authorizationKey = options.authorizationKey || process.env.CRYPTOBADGE_AUTHORIZATION_KEY || null;
+    this.authorizationKey =
+      options.authorizationKey ||
+      process.env.CRYPTOBADGE_AUTHORIZATION_KEY ||
+      null;
 
     const headers: { [key: string]: string } = {
-      "X-CryptoBadge-Access-Token": this.accessToken
+      "X-CryptoBadge-Access-Token": this.accessToken,
     };
     if (this.authorizationKey) {
       headers["X-CryptoBadge-Authorization"] = this.authorizationKey;
@@ -58,8 +70,13 @@ export class CryptobadgeClient extends Queries<NormalizedCacheObject> {
       throw new Error("CRYPTOBADGE_SECRET_KEY is required");
     }
 
-    const cipher = Crypto.createCipheriv("aes-256-cbc", this.authorizationKey, Buffer.alloc(16));
-    const encryptedMsg = cipher.update(email, "utf8", "base64") + cipher.final("base64");
+    const cipher = Crypto.createCipheriv(
+      "aes-256-cbc",
+      this.authorizationKey,
+      Buffer.alloc(16),
+    );
+    const encryptedMsg =
+      cipher.update(email, "utf8", "base64") + cipher.final("base64");
     return encryptedMsg.toString();
   }
 }
